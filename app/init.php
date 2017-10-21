d339dc46ec01ad237305b544ff7dbd2c78858cbd
jumper423 <jump.e.r@yandex.ru>
2017-10-21 15:25:53 +0300

    init

--- app/init.php
+++ app/init.php
@@ -1,17 +1,6 @@
 <?php
 
 require(__DIR__ . '/vendor/autoload.php');
-
-$sl = new \core\SL();
-
-$sl->set('db', new \core\Mysql([
-'database' => 'test_task',
-'user' => 'test_task',
-'password' => 'test_task',
-'host' => 'cp_mysql',
-'port' => 3306,
-]));
-
-\core\App::i()->setSL($sl);
+require __DIR__ . '/bootstrap/bootstrap.php';
 
 \core\App::i()->getDB()->query(file_get_contents(__DIR__ . '/migration.sql'));
